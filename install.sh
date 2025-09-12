e1334565308fa101c6e4fc9966a9e188af7a4eaa
Parveen Chahal <hello@parveen.me>
2025-09-11 19:37:26 -0700

    Fixes for system installation

--- install.sh
+++ install.sh
@@ -12,16 +12,24 @@ test -n "$GOBIN" && binpath="$GOBIN" || {
 
 test -z "$binpath" && { echo "Could not identify Go bin directory."; exit 1; }
 
+use_sudo="false"
+
 for opt do
   value=$(expr "x$opt" : 'x[^=]*=\(.*\)')
   case "$opt" in
-  --installation-path=*) installation_path="$value";
+  --installation-path=*) installation_path="$value"
   ;;
-  *) echo "Unknown argument: $opt"; exit 1;
+  --sudo) use_sudo="true"
+  ;;
+  *) echo "Unknown argument: $opt"; exit 1
   ;;
   esac
 done
 test -e "$installation_path" || mkdir -p "$installation_path"
 test -d "$installation_path" || { echo "'$installation_path' installation path should be a directory"; exit 1; }
 
-cp "$binpath/pflags" "$installation_path"
+if [ "$use_sudo" = "true" ]; then
+  sudo cp "$binpath/pflags" "$installation_path"
+else
+  cp "$binpath/pflags" "$installation_path"
+fi

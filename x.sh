db6fd42a8bd252af29ab286d262060d042907dca
Parveen Chahal <hello@parveen.me>
2025-09-20 02:38:24 -0700

    Delete x.sh

--- x.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/data-disk/pflags/pflags --name "$0" parse ---- -s a -l abc -t string -r --default d1 --regex '.*' --default d2 -h 'ABC\nxyz' -- -s x -t number -- -s p -t bool ---- "$@"
